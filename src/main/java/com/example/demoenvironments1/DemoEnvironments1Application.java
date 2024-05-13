1910643791545a6f6bccd65eb86fc82eb42fec58
Lutti Davide <luttidavide004@gmail.com>
2024-05-13 20:43:17 +0200

    first commit

---
+++ src/main/java/com/example/demoenvironments1/DemoEnvironments1Application.java
@@ -0,0 +1,13 @@
+package com.example.demoenvironments1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEnvironments1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEnvironments1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoenvironments1/DemoEnvironments1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoenvironments1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEnvironments1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
